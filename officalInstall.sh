33df2eaddbddfca019254a9322dd0a092e80db85
MoritzGruber <moritzgruber@yahoo.de>
2017-05-29 16:49:49 +0200

    splitted into two files, offical and myoInstall, fixed docker sudo problem

--- officalInstall.sh
+++ officalInstall.sh
@@ -484,7 +484,4 @@ do_install() {
 do_install
 
 
-# install docker compose 
-curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
