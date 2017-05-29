5303c767676793c1e932b418494a841646473644
MoritzGruber <moritzgruber@yahoo.de>
2017-05-29 17:53:18 +0200

    added settings for /etc/default/docker

--- myInstall.sh
+++ myInstall.sh
@@ -1,5 +1,7 @@
 
 sudo usermod -aG docker "$(whoami)"
+cat defaultdocker | sudo tee /etc/default/docker
+
 
 # install docker compose 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
