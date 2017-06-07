0594f3b8aca426c0893b75e86a342180c2251239
Moritz Gruber <mg166@hdm-stuttgart.de>
2017-06-07 18:43:49 +0200

    Update docker.sh, add docker-compose for debian

--- debian8/docker.sh
+++ debian8/docker.sh
@@ -6,3 +6,7 @@ add-apt-repository \
    stable"
 apt-get update
 apt-get install docker-ce -y
+##docker compose
+curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
